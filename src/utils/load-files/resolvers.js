3dfd67ea1285507f8ac4109f0bd6fd04a8fa43d7
Vinicius Reis <luiz.vinicius73@gmail.com>
2019-03-28 00:05:30 -0300

    🎨 change .resolver to .resolvers

--- src/utils/load-files/resolvers.js
+++ src/utils/load-files/resolvers.js
@@ -7,13 +7,13 @@ const { loadFilesNames, loadFiles, makeGetName } = require('./common')
  * @param {string} dirname
  * @returns {Promise<Array<String>>}
  */
-const loadResolversFilesNames = loadFilesNames('**/**.resolver.js')
+const loadResolversFilesNames = loadFilesNames('**/**.resolvers.js')
 
 /**
  * @param {String} fileName
  * @returns {String}
  */
-const getName = makeGetName('.resolver.js')
+const getName = makeGetName('.resolvers.js')
 
 const loadResolvers = dirName => {
   return composeP(loadFiles(getName, dirName), loadResolversFilesNames)(dirName)
