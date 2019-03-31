7f8a27fb9a3a980509d794d0c8f0c62490dadbc4
Vinicius Reis <luiz.vinicius73@gmail.com>
2019-03-31 11:48:49 -0300

    💡 add doc reference link

--- src/server/directives.js
+++ src/server/directives.js
@@ -1,6 +1,8 @@
 const loadDirectives = require('../utils/load-files/directives')
 const { resolve } = require('path')
 
+// https://www.apollographql.com/docs/graphql-tools/schema-directives
+
 module.exports = {
   loadDirectives: () => loadDirectives(resolve(__dirname, '../directives'))
 }
