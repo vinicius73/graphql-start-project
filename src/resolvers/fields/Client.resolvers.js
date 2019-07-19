c05801f0810512cccf16a8803f580f606f7e38e3
Vinicius Reis <luiz.vinicius73@gmail.com>
2019-07-19 10:47:00 -0300

    ⬆️ update devDependencies

--- src/resolvers/fields/Client.resolvers.js
+++ src/resolvers/fields/Client.resolvers.js
@@ -2,6 +2,7 @@ module.exports = {
   products: ({ id }, args, { services }) => {
     return services.db('products').where({ client_id: id })
   },
+  // eslint-disable-next-line camelcase
   user: ({ user_id }, args, { dataLoaders }) => {
     return dataLoaders.users.load(user_id)
   }
--- src/resolvers/fields/Product.resolvers.js
+++ src/resolvers/fields/Product.resolvers.js
@@ -1,4 +1,5 @@
 module.exports = {
+  // eslint-disable-next-line camelcase
   client: ({ client_id }, args, { dataLoaders }) => {
     return dataLoaders.clients.load(client_id)
   }
